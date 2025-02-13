1c18b3d08c90e8dbc0efc141514c2ef15006b6ae
Lisandro Andres <163011670+DeltaSasaki@users.noreply.github.com>
2025-02-12 22:03:17 -0400

    Update reset_password.php

--- social_network/contraseña/reset_password.php
+++ social_network/contraseña/reset_password.php
@@ -9,14 +9,18 @@ if (!isset($_SESSION['email_verificacion'])) {
 }
 
 $data = json_decode(file_get_contents('php://input'), true);
-$password = hash('sha256', $data['password']);
+$password = $data['password'];
 $email = $_SESSION['email_verificacion'];
 
-try {
-    $stmt = $pdo->prepare("UPDATE usuarios SET contrasena = ?, codigo_recuperacion = NULL, fecha_expiracion_codigo = NULL WHERE email = ?");
-    $stmt->execute([$password, $email]);
+// Encripta la nueva contraseña
+$contrasena_hashed = password_hash($password, PASSWORD_BCRYPT);
 
-    if ($stmt->rowCount() > 0) {
+try {
+    $stmt = $conn->prepare("UPDATE usuarios SET contrasena = ?, codigo_recuperacion = NULL, fecha_expiracion_codigo = NULL WHERE email = ?");
+    $stmt->bind_param("ss", $contrasena_hashed, $email);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
         echo json_encode(['success' => true]);
     } else {
         echo json_encode(['success' => false, 'message' => 'Error al restablecer la contraseña.']);
